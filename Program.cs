7f401652d071ef93fad820409aafa421dd0ddc46
hienghj <luuvanhien557@gmail.com>
2026-03-02 20:08:44 +0700

    Fix: rename port to dbPort to avoid CS0136 variable conflict

--- Program.cs
+++ Program.cs
@@ -19,8 +19,8 @@ else if (connStr.StartsWith("postgres://") || connStr.StartsWith("postgresql://"
     // Convert Render/Railway URL format to Npgsql format
     var uri = new Uri(connStr);
     var userInfo = uri.UserInfo.Split(':');
-    var port = uri.Port > 0 ? uri.Port : 5432;
-    connStr = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+    var dbPort = uri.Port > 0 ? uri.Port : 5432;
+    connStr = $"Host={uri.Host};Port={dbPort};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
 }
 
 builder.Services.AddDbContext<AppDbContext>(options =>
